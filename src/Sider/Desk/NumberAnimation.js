458904684087f5d86c6d8fc13242df579d865ed2
ysczyl <1239864102@qq.com>
2018-11-16 13:16:59 +0800

    11/16

--- src/Sider/Desk/NumberAnimation.js
+++ src/Sider/Desk/NumberAnimation.js
@@ -2,13 +2,26 @@ import { Button  } from 'antd';
 import React, { Component } from 'react';
 import './../../css/Sider/desk.css';
 export default class NumberAnimation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      height:false,
+    };
+  }
+  animate(){
+    this.setState({height:!this.state.height});
+    console.log(this.state.height)
+  }
   render() {
-    function animation() {
-        
-    }
+  	const animate = {
+  		'animation':this.state.height?'MoveBefore 1s linear':'MoveAfter 1s linear',
+      height:this.state.height?'500px':'30px',
+  		'animation-fill-mode':'forwards',
+  		'animation-iteration-count':'1',
+  	}
     return (
-      <div className='animateBox'>
-        <Button className='animateButton'></Button>
+      <div className='animateBox' style={animate}>
+        <Button className='animateButton' onClick={this.animate.bind(this)}></Button>
       </div>
     );
   }
