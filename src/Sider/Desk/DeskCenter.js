08a420bdd78211fe58f83aa32958342d1409b70a
DESKTOP-JJI8FV6\WP-S10 <1239864102@qq.com>
2018-11-18 19:44:53 +0800

    添加工作台Center部分

---
+++ src/Sider/Desk/DeskCenter.js
@@ -0,0 +1,15 @@
+import { Button  } from 'antd';
+import React, { Component } from 'react';
+import './../../css/Sider/desk.css';
+export default class DeskCenter extends React.Component {
+  render() {
+    return (
+      <div>
+          <div className='DeskCenterBox1'></div>
+          <div className='DeskCenterBox2'></div>
+          <div className='DeskCenterBox3'></div>
+          <div className='DeskCenterBox4'></div>
+      </div>
+    );
+  }
+}
--- src/Sider/desk.js
+++ src/Sider/desk.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import './../css/Sider/desk.css';
 import TopNumber from './Desk/TopNumber.js';
 import NumberAnimation from './Desk/NumberAnimation.js';
+import DeskCenter from './Desk/DeskCenter.js';
 export default class Desk extends React.Component {
   render() {
     return (
       <div>
         <TopNumber />
         <NumberAnimation />
+        <DeskCenter />
       </div>
     );
   }
