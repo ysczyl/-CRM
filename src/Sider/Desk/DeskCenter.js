5a1cf1ad7051d41f721c0e6e5ead36704071ae40
ysczyl <1239864102@qq.com>
2018-11-18 23:46:20 +0800

    添加DeskAnimate表格

--- src/Sider/Desk/DeskCenter.js
+++ src/Sider/Desk/DeskCenter.js
@@ -4,11 +4,19 @@ import './../../css/Sider/desk.css';
 export default class DeskCenter extends React.Component {
   render() {
     return (
-      <div>
-          <div className='DeskCenterBox1'></div>
-          <div className='DeskCenterBox2'></div>
-          <div className='DeskCenterBox3'></div>
-          <div className='DeskCenterBox4'></div>
+      <div> 
+          <div className='DeskCenterBox1'>
+              <div className='DeskCenterBoxTOP'></div>
+          </div>
+          <div className='DeskCenterBox2'>
+              <div className='DeskCenterBoxTOP'></div>
+          </div>
+          <div className='DeskCenterBox3'>
+              <div className='DeskCenterBoxTOP'></div>
+          </div>
+          <div className='DeskCenterBox4'>
+              <div className='DeskCenterBoxTOP'></div>
+          </div>
       </div>
     );
   }
--- src/Sider/Desk/NumberAnimation.js
+++ src/Sider/Desk/NumberAnimation.js
@@ -1,6 +1,8 @@
-import { Button  } from 'antd';
+import { Button ,Input ,Select} from 'antd';
 import React, { Component } from 'react';
 import './../../css/Sider/desk.css';
+const InputGroup = Input.Group;
+const Option = Select.Option;
 export default class NumberAnimation extends React.Component {
   constructor(props) {
     super(props);
@@ -9,7 +11,7 @@ export default class NumberAnimation extends React.Component {
       flag:false,
     };
   }
-  animate(){
+  animate(){  
     this.setState({height:!this.state.height});
     console.log(this.state.height)
   }
@@ -20,13 +22,45 @@ export default class NumberAnimation extends React.Component {
   render() {
   	const animate = {
   		'animation':this.state.height?'MoveBefore 1s linear':'MoveAfter 1s linear',
-      height:this.state.height?'500px':'30px',
+      height:this.state.height?'500px':'40px',
   		'animation-fill-mode':'forwards',
   		'animation-iteration-count':'1',
   	}
     return (
       <div className='animateBox' style={animate}>
+        <div className='animateTop'>
+        <strong className='topSpan'>客户数据监控详情</strong>
         <Button className='animateButton' onClick={this.animate.bind(this)}></Button>
+        </div>
+        <div className='animateCenter'>
+            <strong style={{fontSize:20,padding:'15px',float:'left'}}>查看范围:</strong>
+            <InputGroup compact style={{padding:'15px'}}>
+              <Select defaultValue="Option2-1">
+              <Option value="Option2-1">公司</Option>
+              <Option value="Option2-2">Option2-2</Option>
+              </Select>
+            </InputGroup>
+              <table border="1" className='table'>
+              <tr className='tableTR' style={{height:50}}>
+              <td>客户经理</td>
+              <td>当前客户总数</td>
+              <td>今日新客户</td>
+              <td>今日有效新客户</td>
+              <td>今日通话新客户</td>
+              <td>今日放弃客户</td>
+              <td>昨日回库客户</td>
+              </tr>
+              <tr>
+              <td>row 2, cell 1</td>
+              <td>row 2, cell 2</td>
+              <td>row 2, cell 1</td>
+              <td>row 2, cell 2</td>
+              <td>row 2, cell 1</td>
+              <td>row 2, cell 2</td>
+              <td>row 2, cell 1</td>
+              </tr>
+            </table>
+        </div>
       </div>
     );
   }
